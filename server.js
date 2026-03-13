90122852a2281ec0896101c28b9ccf3a23287f44
Leonardotrentini <leozikao50@gmail.com>
2026-03-13 03:18:57 -0300

    CorreÃ§Ãµes: RemoÃ§Ã£o de duplicaÃ§Ãµes na pÃ¡gina 13, restauraÃ§Ã£o do mapa animado original e organizaÃ§Ã£o do conteÃºdo

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const mimeTypes = {
   '.json': 'application/json',
   '.png': 'image/png',
   '.jpg': 'image/jpg',
+  '.jpeg': 'image/jpeg',
   '.gif': 'image/gif',
   '.svg': 'image/svg+xml',
   '.ico': 'image/x-icon'
